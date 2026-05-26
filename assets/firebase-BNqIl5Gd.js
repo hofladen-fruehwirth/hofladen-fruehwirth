16430b9ced4924d00bb64bc0ceec13c851aa2881
blqnk3d <blqn3kd@blqde.com>
2026-05-26 22:54:04 +0200

    Updates

--- assets/firebase-BNqIl5Gd.js
+++ assets/firebase-BNqIl5Gd.js
@@ -1,4 +1,4 @@
-import{A as a,o as e}from"./index.esm-BRL0tNP0.js";var o="firebase",i="12.13.0";/**
+import{A as a,o as e}from"./index.esm-BRL0tNP0.js";var i="firebase",o="12.13.0";/**
  * @license
  * Copyright 2020 Google LLC
  *
@@ -13,4 +13,4 @@ import{A as a,o as e}from"./index.esm-BRL0tNP0.js";var o="firebase",i="12.13.0";
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */a(o,i,"app");const r={apiKey:"AIzaSyC9E200_OabRASroocxLHt7kjawi6zi0-U",authDomain:"hofladen-28da1.firebaseapp.com",projectId:"hofladen-28da1",storageBucket:"hofladen-28da1.firebasestorage.app",messagingSenderId:"611524304119",appId:"1:611524304119:web:6a12d100042034fe2647f2"},s=e(r);export{s as a};
+ */a(i,o,"app");const r={apiKey:"AIzaSyC9E200_OabRASroocxLHt7kjawi6zi0-U",authDomain:"hofladen-28da1.firebaseapp.com",projectId:"hofladen-28da1",messagingSenderId:"611524304119",appId:"1:611524304119:web:6a12d100042034fe2647f2"},s=e(r);export{s as a};
