0732e70b7c5eef141220d49ac95522e74c45add7
blqnk3d <blqn3kd@blqde.com>
2026-05-27 17:55:59 +0200

    change for better url

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHashHistory('/hofladen-fruehwirth/'),
+  history: createWebHashHistory('/'),
   routes: [
     {
       path: '/',
